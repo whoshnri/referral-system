bbce6e8b45bb7fe92aa60773efb73a39aec58ed9
whoshnri <henrybassey2007@gmail.com>
2024-07-03 14:01:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 )
 app.use(express.static(path.join(__dirname, "public")))
 
-/*  Flash and Session*/
+/*  Flash and Session and some other things*/
 app.use(
   session({
     secret: "anysecret",
